6674dcb9403837ea7931ea9ba9b7a75d32d73bf7
ajrob <ajrob.dev@gmail.com>
2014-07-25 00:08:53 -0600

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name utmbApp
+ * @description
+ * # utmbApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('utmbApp', [
+    'ngAnimate'
+  ]);
